4c950088fc80695bc070eaa3b461e5c07f8d7ac3
Anuj Pillai <anuj.mohan@sigmoidanalytics.com>
2023-04-26 17:43:42 +0530

    fix

--- Airflow-Assignment/Airflow-Assignment/dags/sql/insert_employee.sql
+++
@@ -1,7 +0,0 @@
--- insert records
-
-INSERT INTO employee
-VALUES(1,'Annie','Smith','F', DATE '1988-01-09', 'ani@email.com',5000);
-
-INSERT INTO employee
-VALUES(2,'Atul','Singh','M', DATE '2001-01-01', 'atul@email.com',2000);
